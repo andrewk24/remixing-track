44ca054d5b33aaf6c65602ec55d7832896eeeea8
andrewk <42969940+apk1024@users.noreply.github.com>
2021-01-17 10:14:04 -0500

    switched to VehicleBody node

--- VehicleBody.gd
+++ VehicleBody.gd
@@ -1,7 +1,7 @@
-extends KinematicBody
+extends VehicleBody
 
 # Declare member variables here. Examples:
-# var a = 2
+#var speed = 0
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
@@ -10,7 +10,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(.01)
+	if Input.is_action_pressed("ui_left"):
+		set_steering(-.1)
+	elif Input.is_action_pressed("ui_right"):
+		set_steering(.1)
+	else:
+		set_steering(0)
 	if Input.is_action_pressed("ui_up"):
-		#move_and_slide(Vector3(0,0,-1),Vector3(0,1,0))
-		pass
+		set_engine_force(20)
+	else:
+		set_engine_force(0)
+	pass
